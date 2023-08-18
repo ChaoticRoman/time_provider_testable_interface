fbf19249dd05d44ea410c583d198e4e3873153b2
Roman Pavelka <roman.pavelka@siemens.com>
2023-08-18 15:55:51 +0200

    Pass the test

--- src/system_time_provider.cpp
+++ src/system_time_provider.cpp
@@ -2,8 +2,13 @@
 
 #include <ctime>
 
-uint8_t SystemTimeProvider::hour() { return 0; }
+std::tm now() {
+  std::time_t now_epoch = std::time(nullptr);   // get time now
+  return *std::localtime(&now_epoch);
+}
 
-uint8_t SystemTimeProvider::minute() { return 0; }
+uint8_t SystemTimeProvider::hour() { return static_cast<uint8_t>(now().tm_hour); }
 
-uint8_t SystemTimeProvider::second() { return 0; }
+uint8_t SystemTimeProvider::minute() { return static_cast<uint8_t>(now().tm_min); }
+
+uint8_t SystemTimeProvider::second() { return static_cast<uint8_t>(now().tm_sec); }
