7d29442df3d116ba46a3be75f59d8078c1e87ffa
Roman Pavelka <roman.pavelka@siemens.com>
2023-08-18 16:03:05 +0200

    Static linkage to prevent linker name collision

--- src/system_time_provider.cpp
+++ src/system_time_provider.cpp
@@ -2,9 +2,9 @@
 
 #include <ctime>
 
-std::tm now() {
-  std::time_t now_epoch = std::time(nullptr);   // get time now
-  return *std::localtime(&now_epoch);
+static std::tm now() {
+    std::time_t now_epoch = std::time(nullptr);   // get time now
+    return *std::localtime(&now_epoch);
 }
 
 uint8_t SystemTimeProvider::hour() { return static_cast<uint8_t>(now().tm_hour); }
--- tests/test_system_time_provider.cpp
+++ tests/test_system_time_provider.cpp
@@ -7,24 +7,22 @@
 #define SUITE SystemTimeProviderTestSuite
 #define PROVIDER SystemTimeProvider
 
-namespace test {
-    std::tm now() {
-        std::time_t now_epoch = std::time(nullptr);   // get time now
-        return *std::localtime(&now_epoch);
-    }
+static std::tm now() {
+    std::time_t now_epoch = std::time(nullptr);   // get time now
+    return *std::localtime(&now_epoch);
 }
 
 TEST(SUITE, Hour) {
   PROVIDER t;
-  EXPECT_EQ(t.hour(), static_cast<uint8_t>(test::now().tm_hour));
+  EXPECT_EQ(t.hour(), static_cast<uint8_t>(now().tm_hour));
 }
 
 TEST(SUITE, Minute) {
   PROVIDER t;
-  EXPECT_EQ(t.minute(), static_cast<uint8_t>(test::now().tm_min));
+  EXPECT_EQ(t.minute(), static_cast<uint8_t>(now().tm_min));
 }
 
 TEST(SUITE, Second) {
   PROVIDER t;
-  EXPECT_EQ(t.second(), static_cast<uint8_t>(test::now().tm_sec));
+  EXPECT_EQ(t.second(), static_cast<uint8_t>(now().tm_sec));
 }
